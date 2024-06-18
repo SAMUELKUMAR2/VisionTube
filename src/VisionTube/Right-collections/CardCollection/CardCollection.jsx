aa431cd5af128495585a324ee7b96af76d501124
Samuel <samuelkumar721@gmail.com>
2024-06-18 17:51:52 +0530

    Card and CardCollection

---
+++ src/VisionTube/Right-collections/CardCollection/CardCollection.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react'
+import VideoesCards from '../VideoesCollection/VideoesCard.jsx'
+
+import { FetchData } from '../../../API/FetchData.jsx'
+
+
+const CardCollection = () => {
+  const [videoes, setvideoes] = useState([]);
+  const [SelectCategory, SetselectedCategory] = useState('New');
+
+
+  // console.log(FetchData(`search?part=snippet&q=${SelectCategory}`));
+
+ 
+  useEffect(() => {
+    FetchData(`search?part=snippet&q=${SelectCategory}`)
+      .then((data) => {
+        setvideoes(data.items);
+        // console.log(data.items );
+
+      })
+  }, [SelectCategory])
+
+  // if(!SelectCategory?.snippet) return "Loading....";
+  return (
+
+
+    <div className='w-[98vw] h-[98vh] overflow-y-scroll border-2 '>
+      <h5 className='pl-1 font-bold flex '>{SelectCategory}</h5>
+      <VideoesCards videoes={videoes} SelectCategory={SelectCategory} SetselectedCategory={SetselectedCategory} />
+ 
+     
+
+
+    </div>
+  )
+}
+
+export default CardCollection
---
+++ src/VisionTube/Right-collections/CardCollection/Constants.jsx
@@ -0,0 +1,15 @@
+ const categories =[
+    {name:"Universal"},
+    {name:"New Jobs"},
+    {name:"news"},
+    {name:"Health"},
+    {name:"coding"},
+    {name:"Python"},
+    {name:"Html"},
+    {name:"CSS"},
+    {name:"javaScript"},
+    {name:"Java"},
+
+]
+
+export default categories;
